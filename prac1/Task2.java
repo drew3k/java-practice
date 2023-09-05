d547739bfda78da487cf238b043d6865c322e625
def <drystunn@gmail.com>
2023-09-05 23:04:00 +0300

    task 2

---
+++ prac1/Task2.java
@@ -0,0 +1,41 @@
+package prac1;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.println("Введите элементы массива:");
+        int i = 0;
+        do {
+            numbers[i] = scanner.nextInt();
+            i++;
+        } while (i < size);
+
+        int sum = 0;
+        int max = numbers[0];
+        int min = numbers[0];
+
+        i = 0;
+        while (i < size) {
+            sum += numbers[i];
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            i++;
+        }
+
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Максимальный элемент массива: " + max);
+        System.out.println("Минимальный элемент массива: " + min);
+    }
+}
