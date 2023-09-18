a731b000d67aad293daf6b80bc74fccc57aa2ee0
def <drystunn@gmail.com>
2023-09-18 21:16:01 +0300

    added task 1

---
+++ prac4/ex1/Task1.java
@@ -0,0 +1,13 @@
+package prac4.ex1;
+
+enum Season {
+    SPRING, SUMMER, AUTUMN, WINTER
+}
+
+public class Task1 {
+    public static void main(String[] args) {
+        Season favoriteSeason = Season.SUMMER;
+
+        System.out.println("My favorite season is " + favoriteSeason);
+    }
+}
