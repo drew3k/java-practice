6cb8128fb055221cab2bdb07e4d4ca54dc8b7574
def <drystunn@gmail.com>
2023-11-22 19:49:47 +0300

    task

---
+++ prac19/WrongNameException.java
@@ -0,0 +1,15 @@
+package prac19;
+
+public class WrongNameException extends Exception {
+    private final String name;
+
+    WrongNameException(String message, String name) {
+        System.err.println(message);
+        this.name = name;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Invalid name input: " + name;
+    }
+}
